4dcc2ab445b89b20d70c0dfbda093e2484b24989
shify4713 <136832079+shify4713@users.noreply.github.com>
2026-09-16 20:46:24 +0300

    Loader for app-main.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-SEE_FILE
+(function(){
+  var s=document.createElement("script");
+  s.src=(function(){var p=location.pathname||"/";if(p.indexOf(".html")!==-1)p=p.replace(/\/[^\/]*$/,"/");else if(p.charAt(p.length-1)!=="/")p=p+"/";return p})()+"js/app-main.js?v=7";
+  s.async=false;
+  document.head.appendChild(s);
+})();
